b1299e7382834aefcd56b90d9320dfc7b7c5d0a4
kim <kimberleeduval@gmail.com>
2022-02-27 21:29:51 -0500

    set entry

--- phonebook.py
+++ phonebook.py
@@ -9,24 +9,23 @@ menu = """
 3. Delete an entry
 4. List all entries
 5. Quit
-
-
 """
 
 while not(hasQuit):
-print(menu) 
+    print(menu)
+    
+    selected_option = input("what do you want to do (1-5)?")
 
-selected_option = input("What do you want to do (1-5)?")
-
-if selected_option == "2":
-    name = input("what is the contact name?")
-    phone_number = input("what is their phone number?")
-    phonebook[name] = phone_number
-
-    print(phonebook)
-    print("contact added successfully")
-elif selected_option == "1":
-    name = input("what contact's number would you like?")
-    print("Here's their number: ", phonebook[name])
-elif selected_option == "5":
-    hasQuit = True 
+    if  selected_option == "2":
+        name = input("what is the contact's name?")
+        phone_number = input("what is their phone number?")
+        phonebook[name] = phone_number
+        print("contact added successfully")
+    elif selected_option == "1":
+        name = input("what contact's number would you like?")
+        if phonebook.get(name) == None:
+            print("There's no contact with that name... please try again.")
+        else: 
+            print("Here's their number: ", phonebook[name])
+    elif selected_option == "5":
+        hasQuit = True 
